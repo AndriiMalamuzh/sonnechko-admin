f78dd133baa3e37b808a3cbb1b618b5dac54ec74
AndriiMalamuzh <andrey.malamuzh.1994@gmail.com>
2025-07-22 18:03:51 +0200

    add linter

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: ['app'],
+          style: 'kebab-case',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@angular-eslint/prefer-standalone': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': ['error'],
+      '@angular-eslint/prefer-on-push-component-change-detection': ['warn'],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+      eslintPluginPrettierRecommended,
+    ],
+    rules: {
+      '@angular-eslint/template/prefer-self-closing-tags': ['warn'],
+      '@angular-eslint/template/prefer-control-flow': ['warn'],
+      '@angular-eslint/template/interactive-supports-focus': ['warn'],
+      '@angular-eslint/template/click-events-have-key-events': ['warn'],
+    },
+  }
+);
