748cb41a8635551ecbc9c4fb49959197e42e6fdd
AndriiMalamuzh <andrey.malamuzh.1994@gmail.com>
2025-08-03 13:15:53 +0200

    add light and dark themes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,11 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  PLATFORM_ID,
+  OnInit,
+} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,4 +15,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {}
+export class App implements OnInit {
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    this.initializeThemeListener();
+  }
+
+  private initializeThemeListener(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const themeMatchMedia = window.matchMedia('(prefers-color-scheme: dark)');
+
+      const themeChangeListener = (e: any) => {
+        document.body.classList.toggle('dark-theme', e.matches);
+        document.body.classList.toggle('light-theme', !e.matches);
+      };
+
+      themeMatchMedia.addEventListener('change', themeChangeListener);
+      themeChangeListener(themeMatchMedia);
+    }
+  }
+}
